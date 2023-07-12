1201e1305ba07b0fbf6d3a72ad3fc9c5f2ef3f9c
ern0 <ern0@linkbroker.hu>
2023-07-12 13:10:17 +0200

    UI moved to a fn

--- examples/about.rs
+++ examples/about.rs
@@ -11,23 +11,27 @@ fn main() -> Result<(), eframe::Error> {
         ..Default::default()
     };
 
-    // Our application state:
     let mut name = "Arthur".to_owned();
     let mut age = 42;
 
-    eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
+    eframe::run_simple_native("KolorWheel.rs", options, move |ctx, _frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                age += 1;
-            }
-            ui.label(format!("Hello '{name}', age {age}"));
+            panel(ui, &mut name, &mut age);
         });
     })
+}
+
+fn panel(ui: &mut egui::Ui, name: &mut String, age: &mut i32) {
+
+    ui.heading("KolorWheel.rs");
+    ui.horizontal(|ui| {
+        let name_label = ui.label("Your name: ");
+        ui.text_edit_singleline(name).labelled_by(name_label.id);
+    });
+    ui.add(egui::Slider::new(age, 0..=120).text("age"));
+    if ui.button("Click each year").clicked() {
+        *age += 1;
+    }
+    ui.label(format!("Hello '{name}', age {age}"));
+
 }
