87f80dde8d6e649ccba62f1527c565cb27b7b58f
ern0 <ern0@linkbroker.hu>
2023-07-12 13:10:17 +0200

    changed to class

--- examples/about.rs
+++ examples/about.rs
@@ -6,51 +6,65 @@ extern crate kolorwheel;
 
 fn main() -> Result<(), eframe::Error> {
 
+    let window_width = 320.0;
+    let window_height = 240.0;
+
     let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        initial_window_size: Some(egui::vec2(window_width, window_height)),
         ..Default::default()
     };
 
     eframe::run_simple_native("KolorWheel.rs", options, move |ctx, _frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("KolorWheel.rs");
-            panel(ui);
+            let mut app = App::new(ui);
+            app.show_box();
         });
     })
 }
 
-fn panel(ui: &mut egui::Ui) {
+struct App<'u> {
+    ui: &'u mut egui::Ui,
+}
 
-    let (_, painter) = ui.allocate_painter(
-        egui::Vec2::new(
-            ui.available_width(), 
-            ui.available_height(),
-        ),
-        egui::Sense::hover(),
-    );
+impl App<'_> {
 
-    let rect = egui::Rect {
-        min: egui::Pos2{ x: 50.0, y: 50.0 },
-        max: egui::Pos2{ x: 100.0, y: 100.0 },
-    };
+    pub fn new(ui: &mut egui::Ui) -> App {
+        App { ui, }
+    }
 
-    let uniform_rounding = 4.0;
-    let rounding = egui::Rounding {
-        nw: uniform_rounding, 
-        ne: uniform_rounding,
-        sw: uniform_rounding,
-        se: uniform_rounding,
-    };
+    fn show_box(&mut self) {
 
-    let fill = egui::Color32::BLUE;
+        let (_, painter) = self.ui.allocate_painter(
+            egui::Vec2::new(
+                self.ui.available_width(), 
+                self.ui.available_height(),
+            ),
+            egui::Sense::hover(),
+        );
 
-    let stroke = egui::epaint::Stroke{
-        width: uniform_rounding,
-        color: fill,
-    };
+        let rect = egui::Rect {
+            min: egui::Pos2{ x: 50.0, y: 50.0 },
+            max: egui::Pos2{ x: 100.0, y: 100.0 },
+        };
 
-    let rect_shape = egui::epaint::RectShape { rect, rounding, fill, stroke };
-    let rectangle = egui::Shape::Rect(rect_shape);
-    painter.add(rectangle);
+        let uniform_rounding = 4.0;
+        let rounding = egui::Rounding {
+            nw: uniform_rounding, 
+            ne: uniform_rounding,
+            sw: uniform_rounding,
+            se: uniform_rounding,
+        };
 
+        let fill = egui::Color32::BLUE;
+
+        let stroke = egui::epaint::Stroke{
+            width: uniform_rounding,
+            color: fill,
+        };
+
+        let rect_shape = egui::epaint::RectShape { rect, rounding, fill, stroke };
+        let rectangle = egui::Shape::Rect(rect_shape);
+        painter.add(rectangle);
+
+    }
 }
