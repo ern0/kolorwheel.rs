7fe19e6ee8e28fe40791ca44b6537e609bc91d36
ern0 <ern0@linkbroker.hu>
2023-08-14 15:59:47 +0200

    Broken: spin_count tests added

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@ impl<'kw> KolorWheel<'kw> {
         }
     }
 
-    pub fn actual_spinner(&mut self) -> &mut Spinner<'kw> {
+    fn actual_spinner(&mut self) -> &mut Spinner<'kw> {
         &mut self.spinner_vec[self.index]
     }
 
@@ -93,7 +93,7 @@ impl<'kw> KolorWheel<'kw> {
         self
     }
 
-    pub fn spin<T: From<HslColor>>(&mut self, callback: &dyn Fn(T)) {
+    pub fn spin<T: From<HslColor>>(&mut self, callback: &mut dyn FnMut(T)) {
 
         let mut level = 0;
         loop {
@@ -132,94 +132,58 @@ mod tests {
     use assert_float_eq::*;
 
     #[test]
-    fn tst() {
-
-        println!(">>>>>>>>>>>>>>>>>>>>");
-
-        let kw = KolorWheel::new(HslColor::new(0, 100, 50), 4)
-            .fade(FadeMode::Color(HslColor::new(0, 100, 100)))
-            .fade(FadeMode::Gray(50))
-            .fade(FadeMode::Black)
-            .fade(FadeMode::White)
-            .with_hue(SpinMode::Absolute(90))
-            .with_saturation(SpinMode::RelativeIncl(-10))
-            .with_lightness(SpinMode::Offset(&[0, 10]))
+    fn spin_count_4x3() {
+        let mut count = 0;
+        let kw = KolorWheel::new(HslColor::new(0, 0, 0), 4)
             .fork(3)
-            .spin(&|res: HslColor| {
-                println!("-------------{:?}", res);
-            })
+            .spin(&mut|res: HslColor| { count += 1 })
         ;
-        println!("<<<<<<<<<<<<<<<<<<<<");
-
-    }
-}
-
-/*
-    fn slice_u32_to_vec_i32(values: &[u32]) -> Vec<i32> {
-
-        let mut vec_values: Vec<i32> = Vec::new();
-
-        for uvalue in values { 
-            let ivalue = *uvalue as i32;
-            vec_values.push(ivalue) 
-        };
-
-        return vec_values;
+        assert_eq!(count, 12);
     }
 
-    fn next_from_vector(values: Vec<i32>, counter: &mut usize) -> f32 {
-
-        let mut result = values[*counter] as f32;
-        *counter += 1;
-        if *counter == values.len() {
-            *counter = 0;
-        }
-
-        return result;
+    #[test]
+    fn spin_count_1x5() {
+        let mut count = 0;
+        let kw = KolorWheel::new(HslColor::new(0, 0, 0), 1)
+            .fork(5)
+            .spin(&mut|res: HslColor| { count += 1 })
+        ;
+        assert_eq!(count, 5);
     }
 
-    fn spin_stored_hsl(&mut self) {
-
-        Self::spin_stored_channel(&mut self.h, &self.h_spin, &mut self.h_spin_counter);
-        Self::spin_stored_channel(&mut self.s, &self.s_spin, &mut self.s_spin_counter);
-        Self::spin_stored_channel(&mut self.l, &self.l_spin, &mut self.l_spin_counter);
-    }
-
-    fn spin_stored_channel(channel_value: &mut f32, channel_spin: &Spin, channel_counter: &mut usize) {
-
-        if let Spin::Stored(values) = channel_spin {
-            *channel_value = Self::next_from_vector(values.to_vec(), channel_counter);
-        }
-    }
-
-    fn spin_calculated_hsl(&mut self) {
-
-        Self::spin_calculated_channel(&mut self.h, &self.h_spin, &mut self.h_spin_counter);
-        Self::spin_calculated_channel(&mut self.s, &self.s_spin, &mut self.s_spin_counter);
-        Self::spin_calculated_channel(&mut self.l, &self.l_spin, &mut self.l_spin_counter);
-    }
-
-    fn spin_calculated_channel(channel_value: &mut f32, channel_spin: &Spin, channel_counter: &mut usize) {
-
-        if let Spin::Calculated(channel_inc) = channel_spin {
-            *channel_value += channel_inc;
-        }
-    }
-
-    fn offset_hsl(&mut self) {
-
-        Self::offset_channel(&mut self.h, &self.h_offset, &mut self.h_offset_counter);
-        Self::offset_channel(&mut self.s, &self.s_offset, &mut self.s_offset_counter);
-        Self::offset_channel(&mut self.l, &self.l_offset, &mut self.l_offset_counter);
-    }
-
-    fn offset_channel(channel_value: &mut f32, channel_offset: &Offset, channel_counter: &mut usize) {
-
-        if let Offset::OffsetVec(values) = channel_offset {
-            *channel_value += Self::next_from_vector(values.to_vec(), channel_counter);
-        }
+    #[test]
+    fn spin_count_6x1() {
+        let mut count = 0;
+        let kw = KolorWheel::new(HslColor::new(0, 0, 0), 6)
+            .fork(1)
+            .spin(&mut|res: HslColor| { count += 1 })
+        ;
+        assert_eq!(count, 6);
     }
 
 }
 
-*/
+
+    // #[test]
+    // fn tst() {
+
+    //     println!(">>>>>>>>>>>>>>>>>>>>");
+
+    //     let kw = KolorWheel::new(HslColor::new(0, 100, 50), 4)
+    //         .fade(FadeMode::Color(HslColor::new(0, 100, 100)))
+    //         .fade(FadeMode::Gray(50))
+    //         .fade(FadeMode::Black)
+    //         .fade(FadeMode::White)
+    //         .with_hue(SpinMode::Absolute(90))
+    //         .with_saturation(SpinMode::RelativeIncl(-10))
+    //         .with_lightness(SpinMode::Offset(&[0, 10]))
+    //         .fork(3)
+    //         .spin(&|res: HslColor| {
+    //             println!("-------------{:?}", res);
+    //         })
+    //     ;
+    //     println!("<<<<<<<<<<<<<<<<<<<<");
+
+    // }
+
+
