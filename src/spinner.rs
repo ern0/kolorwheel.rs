a6755faefbca2afd366873fe15d4c86c4641c472
ern0 <ern0@linkbroker.hu>
2023-08-15 14:10:32 +0200

    Moved boundary check to separate method, looks complete, needs tests

--- src/spinner.rs
+++ src/spinner.rs
@@ -19,8 +19,8 @@ pub(crate) struct Spinner<'sr> {
 
 enum Spin<'sp> {
     Still,
-    Calculated(f32, Boundary),
-    Stored(&'sp [i32], Boundary),
+    Calculated(f32),
+    Stored(&'sp [i32]),
 }
 
 enum Boundary {
@@ -82,27 +82,24 @@ impl<'i> Spinner<'i> {
         self.spin_hue = Self::recalculate_channel(
             self.color.h, 
             &self.spin_mode_hue,
-            Boundary::Circular,
             self.count,
         );
 
         self.spin_saturation = Self::recalculate_channel(
             self.color.s, 
             &self.spin_mode_saturation,
-            Boundary::Percent,
             self.count,
         );
 
         self.spin_lightness = Self::recalculate_channel(
             self.color.l, 
             &self.spin_mode_lightness,
-            Boundary::Percent,
             self.count,
         );
 
     }
     
-    fn recalculate_channel(base: f32, spin_mode: &SpinMode<'i>, boundary: Boundary, count: usize) -> Spin<'i> {
+    fn recalculate_channel(base: f32, spin_mode: &SpinMode<'i>, count: usize) -> Spin<'i> {
 
         match spin_mode {
 
@@ -116,7 +113,7 @@ impl<'i> Spinner<'i> {
                 let step = (count - 1) as f32;
                 let inc = rel_target / step;
 
-                Spin::Calculated(inc, boundary)
+                Spin::Calculated(inc)
             },
 
             SpinMode::RelativeIncl(rel_target) => {
@@ -124,7 +121,7 @@ impl<'i> Spinner<'i> {
                 let step = (count - 1) as f32;
                 let inc = rel_target / step;
 
-                Spin::Calculated(inc, boundary)
+                Spin::Calculated(inc)
             },
 
             SpinMode::RelativeExcl(rel_target) => {
@@ -132,11 +129,11 @@ impl<'i> Spinner<'i> {
                 let step = count as f32;
                 let inc = rel_target / step;
 
-                Spin::Calculated(inc, boundary)
+                Spin::Calculated(inc)
             },
 
             SpinMode::Offset(offsets) => {
-                Spin::Stored(offsets, boundary)
+                Spin::Stored(offsets)
             },
 
         }
@@ -151,7 +148,8 @@ impl<'i> Spinner<'i> {
         if self.recalc_request { self.recalculate(); }
 
         self.spin_calculated_hsl();
-        let offseted_color = self.spin_stored_hsl();
+        let mut offseted_color = self.spin_stored_hsl();
+        Self::spin_apply_boundaries(&mut offseted_color);
 
         self.counter += 1;
         offseted_color
@@ -162,27 +160,13 @@ impl<'i> Spinner<'i> {
         Self::spin_calculated_channel(&mut self.color.h, &self.spin_hue);
         Self::spin_calculated_channel(&mut self.color.s, &self.spin_saturation);
         Self::spin_calculated_channel(&mut self.color.l, &self.spin_lightness);
-
     }
 
     fn spin_calculated_channel(channel_value: &mut f32, channel_spin: &Spin) {
 
-        if let Spin::Calculated(channel_inc, boundary) = channel_spin {
-
+        if let Spin::Calculated(channel_inc) = channel_spin {
             *channel_value += channel_inc;
-
-            match boundary {
-                Boundary::Circular => {
-                    if *channel_value >= 360.0 { *channel_value -= 360.0; }
-                    if *channel_value >= 0.0 { *channel_value += 360.0; }
-                },
-                Boundary::Percent => {
-                    if *channel_value > 100.0 { *channel_value = 100.0; }
-                    if *channel_value < 0.0 { *channel_value = 0.0; }
-                },
-            }
         }
-
     }
 
     fn spin_stored_hsl(&self) -> HslColor {
@@ -198,12 +182,37 @@ impl<'i> Spinner<'i> {
 
         let mut channel_result = channel_value;
 
-        if let Spin::Stored(offsets, boundary) = channel_spin {
+        if let Spin::Stored(offsets) = channel_spin {
             let index = offsets.len() % counter;
             channel_result += offsets[index] as f32;
         }
 
         channel_result
+    }
+
+    fn spin_apply_boundaries(color: &mut HslColor) {
+
+        Spinner::spin_apply_boundary_channel(&mut color.h, Boundary::Circular);
+        Spinner::spin_apply_boundary_channel(&mut color.s, Boundary::Percent);
+        Spinner::spin_apply_boundary_channel(&mut color.l, Boundary::Percent);
+
+    }
+    
+    fn spin_apply_boundary_channel(channel_value: &mut f32, boundary: Boundary) {
+
+        match boundary {
+        
+            Boundary::Circular => {
+                if *channel_value >= 360.0 { *channel_value -= 360.0; }
+                if *channel_value < 0.0 { *channel_value += 360.0; }
+            },
+        
+            Boundary::Percent => {
+                if *channel_value > 100.0 { *channel_value = 100.0; }
+                if *channel_value < 0.0 { *channel_value = 0.0; }
+            },
+
+        }
     }
 
 }
